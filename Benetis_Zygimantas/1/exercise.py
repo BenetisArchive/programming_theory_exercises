97129d98d37df46da2e8b724c056226081187a4e
zygimantas <zygimantasdev@gmail.com>
2015-02-16 11:17:19 +0200

    function assignment moved up + input data ending symbol changed

--- Benetis_Zygimantas/1/exercise.py
+++ Benetis_Zygimantas/1/exercise.py
@@ -3,18 +3,18 @@ def list_append(lst, item):
     return lst
 
 def input(): 
+    def readFile():
+        f = open('input_data', 'r')
+        return f.read().split('\n')
     fileContent = readFile()
     def groupLinesIntoCases(currentArrayIndex, caseList):
         caseSize = int(fileContent[currentArrayIndex])
-        if caseSize == -1:
+        if caseSize == 0:
             return caseList
         caseStartingIndex = currentArrayIndex + 1
         caseEndingIndex = caseStartingIndex + caseSize
         caseString = fileContent[caseStartingIndex:caseEndingIndex]
         return groupLinesIntoCases(caseEndingIndex, list_append(caseList, '\n'.join([str(x) for x in caseString])))
-    def readFile():
-        f = open('input_data', 'r')
-        return f.read().split('\n')
 
     return groupLinesIntoCases(0, [])
 
